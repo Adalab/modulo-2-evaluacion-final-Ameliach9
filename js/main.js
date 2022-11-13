76404d5e2a15818daa8e8ed3ef4019697b67abfe
Ameliach9 <ale.ameliach@gmail.com>
2022-11-13 19:52:39 +0100

    local storage, finished favorites

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
 //querySelectors
 
 const charactersList = document.querySelector('.js_all_characters');
+const favoritesListEl = document.querySelector('.js-favorites');
+const searchCharacter = document.querySelector('.js-input');
+const form = document.querySelector('.js-form');
+const searchBtn = document.querySelector('.js-button');
+
 
 //Global var
 let characters = [];
-let favorites = [];
+let favoriteCharacters = [];
 
 //FUNCTIONS
-
 
 //Función que pinta para poder pasar parámetros, se usan en el bucle para pintar a todos los personajes
  function renderFirstCharacter (character){
@@ -22,36 +26,86 @@ let favorites = [];
   </li>`;
 }
 
-charactersList.innerHTML = '';
+//charactersList.innerHTML = '';
 
 //bucle recorre el array y va pintando a todos los personajes 
-function renderCharacters (){ 
+function renderCharacters (character){ 
+  let html ='';
   for (let i= 0; i < characters.length; i++) {
-    charactersList.innerHTML += renderFirstCharacter (characters[i]); 
+    html += renderFirstCharacter (characters[i]); 
   }
+  charactersList.innerHTML = html;
+  addCharacterListener();
+}
+
+function addCharacterListener(){
   const charactersArticle = document.querySelectorAll('.js_characters');
   for( const eachCharacterArticle of charactersArticle){
-  eachCharacterArticle.addEventListener('click', (e) => handleClickCharacters(e));
-  console.log(eachCharacterArticle.getAttribute("id"));
-}}
+  eachCharacterArticle.addEventListener('click',  handleClickCharacters);
+ 
+  }}
 
+function renderfavoriteCharacters() {
+  let html = '';
+  for (let i = 0; i < favoriteCharacters.length; i++) {
+    html += renderFirstCharacter(favoriteCharacters[i]);
+  }
+  favoritesListEl.innerHTML = html;
+  
+}
 
 //el querySelectorAll crea un array con todos los elementos article de cada personaje 
 //se crea un bucle for para poner un event listener a cada article 
 // arriba se le asigna la clase a todos los article que se generan
 //se crea el evento al que se le va a añadir el listener
+
+//EVENTOS
 function handleClickCharacters(event) {
 event.currentTarget.classList.toggle('selected');
 console.log(event.currentTarget.id);
 
-const selectedCharacter = characters.find(  
+const selectedCharacter = characters.find (  
   (eachCharacterObj)  => parseInt(eachCharacterObj.char_id) === parseInt(event.currentTarget.id));
 //characters.forEach(characters => {debugger;}) me ayudó a ver qué se le estaba asignando y resolver el undefined 
 console.log(selectedCharacter);
-favorites.push(selectedCharacter);
+//
+const characterInFavIndex = favoriteCharacters.findIndex (  
+  (eachCharacterObj)  => parseInt(eachCharacterObj.char_id) === parseInt(event.currentTarget.id));
+  console.log(characterInFavIndex);
 
-//renderFavoriteCharacters(); usaré esto como función luego 
+  if(characterInFavIndex === -1){
+    //if(!characterInFav)-> mirar la lógica de porque también funciona
+  favoriteCharacters.push(selectedCharacter);
+  
+  localStorage.setItem('favoriteChar', JSON.stringify(favoriteCharacters));
 }
+  //BONUS
+  else{
+    favoriteCharacters.splice( characterInFavIndex, 1 )
+    localStorage.setItem('favoriteChar', JSON.stringify(favoriteCharacters));
+  }
+
+
+
+renderCharacters();
+renderfavoriteCharacters(); 
+}
+
+
+
+
+//SEARCH
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+});
+
+searchCharacter.addEventListener('input', () => {
+  const userSearch = searchCharacter.value.toLowerCase();
+  console.log(userSearch);
+  const filteredCharacters = characters.filter((eachCharacter) => eachCharacter.name.toLowerCase().includes(userSearch));
+  renderCharacters(filteredCharacters);
+});
 
 
 //API
@@ -62,4 +116,7 @@ fetch('https://breakingbadapi.com/api/characters')
     renderCharacters ();
   });
 
-  //Events
+  const savedFav = JSON.parse(localStorage.getItem('favoriteChar'));
+  console.log(savedFav);
+  favoriteCharacters = savedFav
+  renderfavoriteCharacters();
