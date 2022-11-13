8747570de6b8f44ff397dd67452f3342c05ce418
Ameliach9 <ale.ameliach@gmail.com>
2022-11-13 11:12:30 +0100

    fixed asigned id

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ let favorites = [];
 //Función que pinta para poder pasar parámetros, se usan en el bucle para pintar a todos los personajes
  function renderFirstCharacter (character){
   return `
-  <li class="characters__Article js_characters" >
-  <article class="characters__Article__Item" id="=${character.char_id}">
+  <li class="characters__Article js_characters" id="${character.char_id}" >
+  <article class="characters__Article__Item">
       <img src="${character.img}" alt="" class="characters__img">
       <h3 class="characters__name">${character.name}</h3>
       <p class="characters__status">${character.status}</p>
@@ -30,9 +30,10 @@ function renderCharacters (){
     charactersList.innerHTML += renderFirstCharacter (characters[i]); 
   }
   const charactersArticle = document.querySelectorAll('.js_characters');
-  for( const eachCharacterArticle of charactersArticle)
-  eachCharacterArticle.addEventListener('click', handleClickCharacters);
-}
+  for( const eachCharacterArticle of charactersArticle){
+  eachCharacterArticle.addEventListener('click', (e) => handleClickCharacters(e));
+  console.log(eachCharacterArticle.getAttribute("id"));
+}}
 
 
 //el querySelectorAll crea un array con todos los elementos article de cada personaje 
@@ -41,11 +42,11 @@ function renderCharacters (){
 //se crea el evento al que se le va a añadir el listener
 function handleClickCharacters(event) {
 event.currentTarget.classList.toggle('selected');
-console.log(event.currentTarget.char_id);
+console.log(event.currentTarget.id);
 
 const selectedCharacter = characters.find(  
-  (eachCharacterObj)  => eachCharacterObj.id === event.currentTarget.char_id);
-
+  (eachCharacterObj)  => parseInt(eachCharacterObj.char_id) === parseInt(event.currentTarget.id));
+//characters.forEach(characters => {debugger;}) me ayudó a ver qué se le estaba asignando y resolver el undefined 
 console.log(selectedCharacter);
 favorites.push(selectedCharacter);
 
